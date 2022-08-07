3e8c8a98d3adf5685cd5f3ac24041dd9a3df4fae
Dmitry Permyakov <taren4ik@mail.ru>
2022-08-07 16:53:01 +1000

    change tests.py and views.py

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -12,13 +12,13 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.guest_client.get('/second/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('/second/')
         self.assertContains(response, 'А это вторая страница!')
